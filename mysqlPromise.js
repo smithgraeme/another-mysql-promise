4e67609905eecb4c31eb348823f42e35175dd3ff
Graeme Smith <graeme.s@outlook.com>
2018-11-11 17:25:55 -0700

    tidy up spacing

--- mysqlPromise.js
+++ mysqlPromise.js
@@ -1,36 +1,35 @@
 const mysql = require('mysql');
 
 class mysqlPromise {
-    constructor(config) {
-        this.connection = mysql.createConnection(config);
+
+    constructor(configuration) {
+        this.connection = mysql.createConnection(configuration);
     }
 
     query(query, queryArguments) {
-
         return new Promise((resolve, reject) => {
+            this.connection.query(query, queryArguments, (error, result) => {
 
-            this.connection.query(query, queryArguments, (err, result) => {
-
-                if (err) {
-                    return reject(err);
+                if (error) {
+                    return reject(error);
                 }
-
-                resolve(result);
+                else {
+                    resolve(result);
+                }
             });
         });
     }
 
     close() {
-
         return new Promise((resolve, reject) => {
+            this.connection.end(error => {
 
-            this.connection.end(err => {
-
-                if (err) {
-                    return reject(err);
+                if (error) {
+                    return reject(error);
                 }
-
-                resolve();
+                else {
+                    resolve();
+                }
             });
         });
     }
